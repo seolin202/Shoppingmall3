0d8a3e0073495e098bde8965cb1304ccbd33c6e1
Ryn Suh <seolin202>
2020-02-26 23:06:35 +0900

    first commit

---
+++ api/routes/products.js
@@ -0,0 +1,16 @@
+// express loading
+const express = require('express');
+// express 안에 있는 모듈 중 .Router();를 쓴다
+// 이것을 Router라고 상수화시킨다
+const router = express.Router();
+
+// products와 관련된 api 즉 products CRUD는 여기서 진행
+router.get('/', (req, res) => {
+    res.json({
+        msg: "products data get"
+    });
+});
+
+
+// router 모듈화시켜서 out
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+// express loading
+const express = require('express');
+
+"hello my name is R"
+
+// express 실행하는 것을 app이라고 상수화
+const app = express();
+
+
+// server.js 파일에서 라우트를 만들고 상수화 시킨다
+// require(경로)를 ordersRoute와 productsRoute로 상수화 시킨다
+const productsRoute = require('./api/routes/products');
+const ordersRoute = require('./api/routes/orders');
+
+app.use('/products', productsRoute);
+app.use('/orders', ordersRoute);
+
+
+
+// port는 3000번
+const port = 3000;
+// app중의 .listen module 실행, console.log를 통해 server실행되면 server started.. 라는 string을 보이게 한다
+app.listen(port, console.log("server started.."));
